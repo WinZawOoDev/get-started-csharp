5aa4308baaf197c0fab4d8213efc88d26b1571d2
WinZawOo <winzawoo.dev@gmail.com>
2025-03-13 21:45:08 +0630

    string, numbers, intergers

--- Program.cs
+++ Program.cs
@@ -4,11 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("What is your name?");
-        var name = Console.ReadLine();
-        var currentDate = DateTime.Now;
-        Console.WriteLine($"{Environment.NewLine}Hello, {name}, on {currentDate:d} at {currentDate:t}!");
-        Console.Write($"{Environment.NewLine}Press Enter to exit...");
-        Console.Read();
+        // Console.WriteLine("What is your name?");
+        // var name = Console.ReadLine();
+        // var currentDate = DateTime.Now;
+        // Console.WriteLine($"{Environment.NewLine}Hello, {name}, on {currentDate:d} at {currentDate:t}!");
+        // Console.Write($"{Environment.NewLine}Press Enter to exit...");
+        // Console.Read();
+
+        // Console.WriteLine("Hello David!");
+
+        // var firstFriend = "     Maria   ";
+
+        // firstFriend = firstFriend.TrimStart();
+        // firstFriend = firstFriend.TrimEnd();
+
+        // string secondFriend = "    Sotty    ";
+
+        // string friends = $"My friends are {firstFriend} and {secondFriend.Trim()}";
+
+        // Console.WriteLine(friends);
+        // friends = friends.Replace("Sotty", "Dammys");
+        // Console.WriteLine(friends);
+        // Console.WriteLine(friends.Contains("Sotty"));
+        // Console.WriteLine(friends.ToUpper());
+        // Console.WriteLine(friends.Length);
+
+
+        //Numbers, Intergers
+        int a = 2100000000;
+        int b = 2100000000;
+        long c =  (long)a + (long)b;
+        Console.WriteLine(c);
+
+
     }
 }
